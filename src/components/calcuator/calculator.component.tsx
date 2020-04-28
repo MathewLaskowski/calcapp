fb5b26b6bb1ba7cd125ab7471933cdaf2453e77b
mlaskowski <maciejlaskowsky@gmail.com>
2020-04-28 20:38:54 +0200

    basic calculator functionality

--- src/components/calcuator/calculator.component.tsx
+++ src/components/calcuator/calculator.component.tsx
@@ -8,8 +8,49 @@ import { Row, CalculatorLayout } from './calculator.styles'
 
 import { data } from './data';
 
+let value1: undefined | number
+let value2: undefined | number
+let lastButton: undefined | string
+let operation: undefined | string
+
 const handleClickButton = (label: string): void => {
-  console.log(calculatorButtonParse(label))
+
+  const parseButton: string = calculatorButtonParse(label)
+
+  if (parseButton === 'value') {
+    if (!value1 && !value2) {
+      value1 = parseInt(label)
+    } else if (value1 && !value2 && lastButton !== 'operation') {
+      value1 = parseInt(value1 + label)
+    } else if (!value2 && value1 && lastButton === 'operation') {
+      value2 = parseInt(label)
+    } else if (value2 && value1) {
+      value2 = parseInt(value2 + label)
+    }
+  }
+
+  lastButton = parseButton
+
+  if (parseButton === 'operation') {
+    if (!operation) {
+      operation = label
+    } else if (operation && value1 && value2 && label === '=') {
+      if (operation === '+') {
+        console.log(value1 + value2)
+        value1 = undefined
+        value2 = undefined
+        lastButton = undefined
+        operation = undefined
+      } else if (operation === '-') {
+        console.log(value1 - value2)
+        value1 = undefined
+        value2 = undefined
+        lastButton = undefined
+        operation = undefined
+      }
+    }
+  }
+
 }
 
 const renderRows = () => data.map(item => (
