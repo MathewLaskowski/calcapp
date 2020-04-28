8622e37e0f097ddea4f0d844350c9373224050a1
mlaskowski <maciejlaskowsky@gmail.com>
2020-04-28 20:56:01 +0200

    display operation

--- src/components/calcuator/calculator.component.tsx
+++ src/components/calcuator/calculator.component.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CalculatorButton from '../calculator-button/calculator-button.component';
 import CalculatorResult from '../calculator-result/calculator-result.component';
 
@@ -13,61 +13,73 @@ let value2: undefined | number
 let lastButton: undefined | string
 let operation: undefined | string
 
-const handleClickButton = (label: string): void => {
+const Calculator: React.FC = () => {
 
-  const parseButton: string = calculatorButtonParse(label)
+  const [operationText, setOperationText] = useState()
 
-  if (parseButton === 'value') {
-    if (!value1 && !value2) {
-      value1 = parseInt(label)
-    } else if (value1 && !value2 && lastButton !== 'operation') {
-      value1 = parseInt(value1 + label)
-    } else if (!value2 && value1 && lastButton === 'operation') {
-      value2 = parseInt(label)
-    } else if (value2 && value1) {
-      value2 = parseInt(value2 + label)
+  const handleClickButton = (label: string): void => {
+
+    const parseButton: string = calculatorButtonParse(label)
+
+    if (parseButton === 'value') {
+      if (!value1 && !value2) {
+        value1 = parseInt(label)
+        setOperationText(value1)
+      } else if (value1 && !value2 && lastButton !== 'operation') {
+        value1 = parseInt(value1 + label)
+        setOperationText(value1)
+      } else if (!value2 && value1 && lastButton === 'operation') {
+        value2 = parseInt(label)
+        setOperationText(`${operationText} ${value2}`)
+      } else if (value2 && value1) {
+        value2 = parseInt(value2 + label)
+        setOperationText(`${operationText}${label}`)
+      }
     }
-  }
 
-  lastButton = parseButton
+    lastButton = parseButton
 
-  if (parseButton === 'operation') {
-    if (!operation) {
-      operation = label
-    } else if (operation && value1 && value2 && label === '=') {
-      if (operation === '+') {
-        console.log(value1 + value2)
-        value1 = undefined
-        value2 = undefined
-        lastButton = undefined
-        operation = undefined
-      } else if (operation === '-') {
-        console.log(value1 - value2)
-        value1 = undefined
-        value2 = undefined
-        lastButton = undefined
-        operation = undefined
+    if (parseButton === 'operation') {
+      if (!operation) {
+        operation = label
+        setOperationText(`${operationText} ${operation}`)
+      } else if (operation && value1 && value2 && label === '=') {
+        if (operation === '+') {
+          console.log(value1 + value2)
+          setOperationText(`${operationText} ${label} ${value1 + value2}`)
+          value1 = undefined
+          value2 = undefined
+          lastButton = undefined
+          operation = undefined
+        } else if (operation === '-') {
+          console.log(value1 - value2)
+          setOperationText(`${operationText} ${label} ${value1 + value2}`)
+          value1 = undefined
+          value2 = undefined
+          lastButton = undefined
+          operation = undefined
+        }
       }
     }
   }
 
+  const renderRows = () => data.map(item => (
+    <Row key={JSON.stringify(item)}>
+      {item.map(
+        (element: {label: string, size?: 'big' | 'small' }) => (
+          <CalculatorButton {...element} key={element.label} handleClick={handleClickButton} />
+        )
+      )}
+    </Row>
+  ))
+
+  return (
+    <CalculatorLayout>
+      <CalculatorResult />
+      {renderRows()}
+      <div style={{ fontSize: '2rem', color: 'white' }}>{operationText}</div>
+    </CalculatorLayout>
+  )
 }
-
-const renderRows = () => data.map(item => (
-  <Row key={JSON.stringify(item)}>
-    {item.map(
-      (element: {label: string, size?: 'big' | 'small' }) => (
-        <CalculatorButton {...element} key={element.label} handleClick={handleClickButton} />
-      )
-    )}
-  </Row>
-))
-
-const Calculator: React.FC = () => (
-  <CalculatorLayout>
-    <CalculatorResult />
-    {renderRows()}
-  </CalculatorLayout>
-)
 
 export default Calculator
