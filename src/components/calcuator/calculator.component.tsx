8b97c6abd3025ec694dbe88e7aa4707b162a899c
mlaskowski <maciejlaskowsky@gmail.com>
2020-04-28 18:59:27 +0200

    generate data

--- src/components/calcuator/calculator.component.tsx
+++ src/components/calcuator/calculator.component.tsx
@@ -3,31 +3,19 @@ import CalculatorButton from '../calculator-button/calculator-button.component';
 
 import { Row } from './calculator.styles'
 
+import { data } from './data';
+
+const renderRows = () => data.map(item => (
+  <Row>
+    {item.map(
+      (element: {label: string, size?: 'big' | 'small' }) => <CalculatorButton {...element} />
+    )}
+  </Row>
+))
+
 const Calculator: React.FC = () => (
   <>
-    <Row>
-      <CalculatorButton label='1' />
-      <CalculatorButton label='2' />
-      <CalculatorButton label='3' />
-      <CalculatorButton label='x' />
-    </Row>
-    <Row>
-      <CalculatorButton label='4' />
-      <CalculatorButton label='5' />
-      <CalculatorButton label='6' />
-      <CalculatorButton label='-' />
-    </Row>
-    <Row>
-      <CalculatorButton label='7' />
-      <CalculatorButton label='8' />
-      <CalculatorButton label='9' />
-      <CalculatorButton label='+' />
-    </Row>
-    <Row>
-      <CalculatorButton label='0' size='big' />
-      <CalculatorButton label='.' />
-      <CalculatorButton label='=' />
-    </Row>
+    {renderRows()}
   </>
 )
 
---
+++ src/components/calcuator/data.ts
@@ -0,0 +1,70 @@
+export const data: { label: string, size?: 'big' | 'small'}[][] = [
+  [
+    {
+      label: 'AC'
+    },
+    {
+      label: '[+/-]'
+    },
+    {
+      label: '%'
+    },
+    {
+      label: '/'
+    }
+  ],
+  [
+    {
+      label: '1'
+    },
+    {
+      label: '2'
+    },
+    {
+      label: '3'
+    },
+    {
+      label: 'x'
+    }
+  ],
+  [
+    {
+      label: '4'
+    },
+    {
+      label: '5'
+    },
+    {
+      label: '6'
+    },
+    {
+      label: '-'
+    }
+  ],
+  [
+    {
+      label: '7'
+    },
+    {
+      label: '8'
+    },
+    {
+      label: '9'
+    },
+    {
+      label: '+'
+    }
+  ],
+  [
+    {
+      label: '0',
+      size: 'big'
+    },
+    {
+      label: '.'
+    },
+    {
+      label: '='
+    }
+  ]
+]
