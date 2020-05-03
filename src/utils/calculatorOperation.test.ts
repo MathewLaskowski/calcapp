11f89b8698ab3b925dd936d5a8ea00fc1a4089d3
mlaskowski <maciejlaskowsky@gmail.com>
2020-05-03 21:38:02 +0200

    tests for calculatorOperation

--- src/utils/calculatorOperation.test.ts
+++ src/utils/calculatorOperation.test.ts
@@ -11,20 +11,74 @@ describe('generateOperationText method', () => {
 })
 
 describe('calculatorOperation method', () => {
-  const expectedObject = {
-    operationText: '1',
-    result: undefined,
-    currentOperation: undefined,
-    calculateValue1: 1,
-    calculateValue2: undefined
-  }
   it('should return only value1 and operationText', () => {
+    const expectedObject = {
+      operationText: '1',
+      result: undefined,
+      currentOperation: undefined,
+      calculateValue1: 1,
+      calculateValue2: undefined
+    }
     expect(calculatorOperation(
       '1',
       'value',
       undefined,
       undefined,
       undefined,
+      undefined
+    )).toMatchObject(expectedObject)
+  })
+
+  it('should return result on value1 and value2', () => {
+    const expectedObject = {
+      operationText: '10 + 10 = 20',
+      result: 20,
+      currentOperation: '+',
+      calculateValue1: 10,
+      calculateValue2: 10
+    }
+    expect(calculatorOperation(
+      '=',
+      'operation',
+      10,
+      10,
+      'value',
+      '+'
+    )).toMatchObject(expectedObject)
+  })
+
+  it('should set operation after value1', () => {
+    const expectedObject = {
+      operationText: '10 +',
+      result: undefined,
+      currentOperation: '+',
+      calculateValue1: 10,
+      calculateValue2: undefined
+    }
+    expect(calculatorOperation(
+      '+',
+      'operation',
+      10,
+      undefined,
+      'value',
+      undefined
+    )).toMatchObject(expectedObject)
+  })
+
+  it('should return default object', () => {
+    const expectedObject = {
+      operationText: '',
+      result: undefined,
+      currentOperation: undefined,
+      calculateValue1: undefined,
+      calculateValue2: undefined
+    }
+    expect(calculatorOperation(
+      '+',
+      'operation',
+      undefined,
+      undefined,
+      'value',
       undefined
     )).toMatchObject(expectedObject)
   })
