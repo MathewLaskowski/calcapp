69d9ff4257fb4461dc4cda44fc0daa1df1af5c80
mlaskowski <maciejlaskowsky@gmail.com>
2020-05-06 10:09:57 +0200

    test for CalculatorResult component

--- src/components/calculator-result/calculator-result.component.test.tsx
+++ src/components/calculator-result/calculator-result.component.test.tsx
@@ -8,3 +8,9 @@ it('CalculatorResult should render', () => {
   expect(wrapper.exists()).toBe(true)
 })
 
+it('CalculatorResult should render', () => {
+  const wrapper = shallow(<div><CalculatorResult /></div>)
+  wrapper.setProps({ result: 100 })
+  expect(wrapper.props().result).toEqual(100)
+})
+
