01cba2cd4504c221c25fba54022f62ca2612a0f3
mlaskowski <maciejlaskowsky@gmail.com>
2020-05-07 21:27:29 +0200

    tests for CalculatorButton Component

--- src/components/calculator-button/calculator-button.component.test.tsx
+++ src/components/calculator-button/calculator-button.component.test.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import { create } from 'react-test-renderer'
-import { isElement } from 'react-dom/test-utils';
+import { act, isElement } from 'react-dom/test-utils';
 
 import CalculatorButton from './calculator-button.component';
+
+import { render, unmountComponentAtNode } from "react-dom";
+
+
+let container: Element | null = null;
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  if (container) {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  }
+});
+
 
 it('CalculatorButton should render', () => {
   expect(isElement(
@@ -14,7 +32,7 @@ it('CalculatorButton should render', () => {
 })
 
 it('CalculatorButton should have required props', () => {
-  const mockHandleClick = () => {}
+  const mockHandleClick = jest.fn()
   const testRender = create(
     <CalculatorButton
       label='test'
@@ -24,4 +42,28 @@ it('CalculatorButton should have required props', () => {
     />)
   expect(testRender.root.props.label).toBe('test')
   expect(testRender.root.props.handleClick).toBe(mockHandleClick)
+})
+
+it('onClick on Button should be called', () => {
+  const onClick = jest.fn(label => label)
+  act(() => {
+    render(
+      <CalculatorButton
+        label='test'
+        handleClick={onClick}
+        bold={true}
+        size='big'
+      />, container);
+  });
+
+  const component = document.querySelector('div')
+
+  act(() => {
+    if (component) {
+      component.querySelector('div')?.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+    }
+  })
+
+  expect(onClick).toHaveBeenCalledTimes(1);
+  expect(onClick).toReturnWith('test');
 })
