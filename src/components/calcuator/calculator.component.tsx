45e2af16215fe998233f8e00c5d8bc7bbde6588a
mlaskowski <maciejlaskowsky@gmail.com>
2020-05-13 23:59:20 +0200

    some code refactor

--- src/components/calcuator/calculator.component.tsx
+++ src/components/calcuator/calculator.component.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { observer, inject } from 'mobx-react'
+
+import { CalculatorStoreType } from '../../stores/ClipboardStore';
+
 import CalculatorButton from '../calculator-button/calculator-button.component';
 import CalculatorResult from '../calculator-result/calculator-result.component';
 
@@ -10,42 +13,24 @@ import { Row, CalculatorLayout } from './calculator.styles'
 
 import { data } from './data';
 
-interface CalculatorStore {
-  value1: undefined | number
-  value2: undefined | number
-  lastButton: undefined | string
-  operation: undefined | string
-  setValue1: (value: undefined | number) => void,
-  setValue2: (value: undefined | number) => void,
-  setLastButton: (value: undefined | string) => void,
-  setOperation: (value: undefined | string) => void
-}
-
 interface CalculatorProps {
-  calculatorStore?: CalculatorStore
+  calculatorStore?: CalculatorStoreType
 }
 
 const Calculator: React.FC<CalculatorProps> = (props) => {
 
-  let { value1, value2, lastButton, operation, setValue1, setValue2, setLastButton, setOperation } = props.calculatorStore as CalculatorStore
+  let { value1, value2, lastButton, operation, setValue1,
+    setValue2, setLastButton, setOperation
+  } = props.calculatorStore as CalculatorStoreType
 
   const [operationText, setOperationText] = useState()
   const [result, setResult] = useState()
 
   const handleClickButton = (label: string): void => {
-    const {
-      operationText,
-      result,
-      currentOperation,
-      calculateValue1,
-      calculateValue2
-    } = calculatorOperation(
-      label,
-      calculatorButtonParse(label),
-      value1,
-      value2,
-      lastButton,
-      operation
+    const { operationText, result, currentOperation,
+      calculateValue1, calculateValue2
+    } = calculatorOperation( label, calculatorButtonParse(label),
+      value1, value2, lastButton, operation
     )
 
     setLastButton(calculatorButtonParse(label))
--- src/stores/ClipboardStore.ts
+++ src/stores/ClipboardStore.ts
@@ -1,6 +1,17 @@
 import { observable, action } from "mobx"
 
-export default class CalculatorState {
+export interface CalculatorStoreType {
+  value1: undefined | number
+  value2: undefined | number
+  lastButton: undefined | string
+  operation: undefined | string
+  setValue1: (value: undefined | number) => void,
+  setValue2: (value: undefined | number) => void,
+  setLastButton: (value: undefined | string) => void,
+  setOperation: (value: undefined | string) => void
+}
+
+export default class CalculatorStore {
   @observable value1: undefined | number
   @observable value2: undefined | number
   @observable lastButton: undefined | string
