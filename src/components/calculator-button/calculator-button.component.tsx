f0e4900593cd61908afe38580216e609a6c9c208
mlaskowski <maciejlaskowsky@gmail.com>
2020-05-16 22:35:29 +0200

    add active property to button

--- src/components/calculator-button/calculator-button.component.tsx
+++ src/components/calculator-button/calculator-button.component.tsx
@@ -5,11 +5,19 @@ type CalculatorButtonProps = {
   label: string
   size?: 'big' | 'small',
   bold?: boolean,
+  active?: boolean,
   handleClick(label: string): void
 }
 
-const CalculatorButton: React.FC<CalculatorButtonProps> = ({ label, size, bold, handleClick }): ReactElement => (
-  <Button onClick={() => handleClick(label)} size={size} bold={bold}>{label}</Button>
+const CalculatorButton: React.FC<CalculatorButtonProps> = (props): ReactElement => (
+  <Button
+    onClick={() => props.handleClick(props.label)}
+    size={props.size}
+    bold={props.bold}
+    active={props.active}
+  >
+    {props.label}
+  </Button>
 )
 
 export default CalculatorButton
--- src/components/calculator-button/calculator-button.styles.tsx
+++ src/components/calculator-button/calculator-button.styles.tsx
@@ -2,18 +2,19 @@ import styled from 'styled-components';
 
 type ButtonProps = {
   size?: string,
-  bold?: boolean
+  bold?: boolean,
+  active?: boolean
 }
 
 export const Button = styled.div<ButtonProps>`
-  background-color: #354358;
+  background-color: ${({ active }) => active ? '#6B63FF' : '#354358'};
   color: #D4D5D7;
-  width: ${({size}) => size === 'big' ? '124px' : '55px'};
+  width: ${({ size }) => size === 'big' ? '124px' : '55px'};
   height: 60px;
   display: flex;
   justify-content: center;
   align-items: center;
   margin: 7px;
-  font-weight: ${({bold}) => bold ? 'bold' : 'regular'};
+  font-weight: ${({ bold }) => bold ? 'bold' : 'regular'};
   cursor: pointer;
 `
