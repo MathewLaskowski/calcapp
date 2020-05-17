ec0774db98f267656236e38ce5c978c14564258b
mlaskowski <maciejlaskowsky@gmail.com>
2020-05-17 14:56:26 +0200

    update operation text for float value2

--- src/utils/generateOperationText.ts
+++ src/utils/generateOperationText.ts
@@ -17,6 +17,7 @@ export const generateOperationText: generateOperationTextType = (
 ) => {
   const { calculateValue1: value1, calculateValue2: value2, calculateTypeOfValue1, calculateTypeOfValue2 } = calculatedData
   const valueText1 = typeof value1 !== 'undefined' ? value1 === 0 && calculateTypeOfValue1 === 'float' ? '0.' : value1  : ''
+  const valueText2 = typeof value2 !== 'undefined' ? value2 === 0 && calculateTypeOfValue2 === 'float' ? '0.' : value2  : ''
 
-  return `${valueText1} ${operation ? operation : ''} ${value2 ? value2 : ''} ${result ? `= ${result}` : ''}`.trim()
+  return `${valueText1} ${operation ? operation : ''} ${valueText2} ${result ? `= ${result}` : ''}`.trim()
 }
