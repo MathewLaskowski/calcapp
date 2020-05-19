f59a463a98ed9360f2fa457bec02608aaf478e0b
mlaskowski <maciejlaskowsky@gmail.com>
2020-05-19 23:12:53 +0200

    add menu list component

---
+++ src/components/menu-list/menu-list.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const MenuList = () => <div>List elementów menu</div>
+
+export default MenuList
--- src/components/menu/menu.component.tsx
+++ src/components/menu/menu.component.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { MenuContainer, Title, SubTitle } from './menu.styles'
+import MenuList from '../menu-list/menu-list'
 
 const Menu = () => (
   <MenuContainer>
     <Title>Calculated</Title>
     <SubTitle>Select your calculator</SubTitle>
+    <MenuList />
   </MenuContainer>
 )
 
