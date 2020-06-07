1eeae13154e83e34646e8719dea05a6c80afa8ef
mlaskowski <maciejlaskowsky@gmail.com>
2020-06-07 23:22:46 +0200

    move handleClick method inside menu-list component

--- src/components/menu-list/menu-list.tsx
+++ src/components/menu-list/menu-list.tsx
@@ -3,18 +3,20 @@ import { Layout, Icon } from './menu-list.styles'
 
 type handleClickType = (item: string) => void
 
-const handleClick: handleClickType = item => {
-  console.log(item)
-}
-
 const menuListItems = ['Calculator', 'Apps']
 
-const MenuList = () => (
-  <Layout>
-    {menuListItems.map(el => (
-      <Icon key={el} onClick={() => handleClick(el)}>{el}</Icon>
-    ))}
-  </Layout>
-)
+const MenuList = () => {
 
+  const handleClick: handleClickType = item => {
+    console.log(item)
+  }
+
+  return (
+    <Layout>
+      {menuListItems.map(el => (
+        <Icon key={el} onClick={() => handleClick(el)}>{el}</Icon>
+      ))}
+    </Layout>
+  )
+}
 export default MenuList
