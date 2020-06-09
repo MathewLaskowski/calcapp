6cf9e6af7818f1a2eaaaff082d3378208e2af314
mlaskowski <maciejlaskowsky@gmail.com>
2020-06-09 18:52:41 +0200

    add UIStore to RootStore

--- src/stores/rootStore.ts
+++ src/stores/rootStore.ts
@@ -1,8 +1,11 @@
 import CalculatorStore from './CalculatorStore';
+import UIStore from './UIStore';
 
 export default class RootStore {
   calculatorStore: object
+  uiStore: object
   constructor() {
     this.calculatorStore = new CalculatorStore();
+    this.uiStore = new UIStore();
   }
 }
