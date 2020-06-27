2e50a6b7318e70002a32f8b981693e6330d75c29
mlaskowski <maciejlaskowsky@gmail.com>
2020-06-27 17:35:50 +0200

    connect UIStore to Layout component

--- src/components/layout/layout.component.tsx
+++ src/components/layout/layout.component.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Menu from '../menu/menu.component'
 import Calculator from '../calcuator/calculator.component';
+import { inject, observer } from 'mobx-react';
 import { LayoutContainer } from './layout.styles'
 
-const Layout: React.FC = () => (
+const Layout: React.FC = (props) => {
+  console.log(props)
+  return (
   <LayoutContainer>
     <Menu active='Calculator' />
     <Calculator />
   </LayoutContainer>
-)
+)}
 
-export default Layout
+export default inject('uiStore')(observer(Layout))
